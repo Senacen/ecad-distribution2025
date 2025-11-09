2c21e01493f7f7f529f161e27adca18e3b2f747e
Duy Huynh <duy@donggiang.com>
2025-11-09 14:09:49 +0000

    tick 3

--- riscv/assembly/src/div.s
+++ riscv/assembly/src/div.s
@@ -22,13 +22,21 @@ div:
 
     # for i = n-1...0 do     -- where n is number of bits in N
     li t2, 31 # i
+    msb_scan:
+        srl  t6, a0, t2         # t6 = N >> t2
+        andi t6, t6, 1          # t6 = bit t2 of N
+        bnez t6, msb_found      # if bit set, t2 is MSB
+        addi t2, t2, -1
+        bgez t2, msb_scan       # keep scanning while t2 >= 0
+    msb_found:
+
     li t3, 0 # 
 
     # Get a 1 to shift for the mask
     li t5, 1
 
     for:
-        beq t2, t3, end # if i == 0, break
+        blt t2, t3, end # if i < 0, break
 
         # R := R << 1          -- left-shift R by 1 bit
         slli t1, t1, 1
@@ -48,18 +56,23 @@ div:
             sll t6, t5, t2 # Make a mask in t6 that is a 1 (t5) left shifted by i (t2)
             or t0, t0, t6 # Or the mask with Q to set the ith bit
         endif: 
+        addi t2, t2, -1
+        j for
     end:
 
-    
+    # if denominator != 0, jump to denom_fine
+    bne a1, t3, denom_fine 
 
     denom_0:
         li a0, 0 # set result to 0
-        beq a0, a0, end_denom # Jump to end_denom
+        li a1, 0 # set result to 0
+        j end_denom # Jump to end_denom
     denom_fine:
-        lw a0, 0(t0) # set result to Q
+        addi a0, t0, 0 # set result to Q
+        addi a1, t1, 0 # set mod to R
     end_denom:
 
-    lw a1, 0(t1) # set mod to R
+    
 
     # example of printing inputs a0 and a1
     DEBUG_PRINT a0
